c3697f2e8c7a2baf3b8bc6062d8ab63a55c8af0b
Bolle <marcel.brunner@unity-dev.de>
2025-11-17 19:51:58 +0100

    add 1. function

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const dialog = document.getElementById("pictureDialog");
+const dialogRef = document.getElementById("pictureDialog");
 const dialogPicture = document.getElementById("dialogPicture");
 
+function openPictureDialog(params) {
+    dialogRef.showModal();
+}
+
+function closePictureDialog() {
+    dialogRef.close();
+}
+
+
+
+
+
+// const thumbs = document.querySelectorAll(".thumbnail");
+
+// thumbs.forEach((thumb) => {
+//     thumb.addEventListener("click" , (e) => {
+//         e.preventDefault();
+
+//         const fullImg =thumb.getAttribute("href")
+//         console.log(fullImg);
+//     })
+// })
